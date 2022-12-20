d45949d20b233ed39c47dc352c45eccd6f4f28f0
Lenova <thefbsl@gmail.com>
2022-12-20 12:34:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataSet = Datasource().loadAffirmations()
+        rv_recycle.adapter = ItemAdapter(this, dataSet)
+        rv_recycle.setHasFixedSize(true)
+
+    }
+}
